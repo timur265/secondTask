fc8b4f051c6eb9060460494b5ba30627dd0d153e
Timur Vorobyov <buskettim0902@gmail.com>
2021-11-04 08:28:05 +0500

    chore: creates Typography component

---
+++ src/components/ui-kit/Typography/Typography.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import classes from "./Typography.module.css";
+import cn from "clsx";
+
+type TypographyProps = {
+  variant:
+  | "p"
+  | "h1"
+  | "h2"
+  | "h3"
+  | "h4"
+  | "h5"
+  | "li"
+  | "span"
+  | "label"
+  children: React.ReactChild | string[];
+  className?: string;
+  style?: React.CSSProperties;
+  color?: string;
+  fontWeight?: string;
+};
+
+const Typography = (props: TypographyProps) => {
+  const { variant, children, style } = props;
+  const Component = variant;
+
+  const getClassName = (): string => {
+    const { fontWeight, color, variant, className } = props;
+    return cn({
+      [classes.common]: true,
+      [classes.p]: variant === "p",
+      [classes.h1]: variant === "h1",
+      [classes.h2]: variant === "h2",
+      [classes.h3]: variant === "h3",
+      [classes.h4]: variant === "h4",
+      [classes.h5]: variant === "h5",
+      [classes.li]: variant === "li",
+      [classes.label]: variant === "label",
+      [classes.span]: variant === "span",
+      [classes.white]: color == "white",
+      [classes.black]: color == "black",
+    });
+  };
+
+  return (
+    <Component style={style} className={getClassName()}>
+      {children}
+    </Component>
+  );
+};
+
+export default Typography;
