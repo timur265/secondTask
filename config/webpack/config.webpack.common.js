3d63b1eb1dcb701c4b10233137dce6c9cffab0ed
Timur Vorobyov <buskettim0902@gmail.com>
2021-11-04 07:10:09 +0500

    chore: settings webpack

---
+++ config/webpack/config.webpack.common.js
@@ -0,0 +1,92 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  entry: path.resolve(__dirname, "../..", "src", "index.tsx"),
+  output: {
+    filename: "main.js",
+    path:path.resolve(__dirname, "../..", "dist"),
+  },
+  devServer: {
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.ts$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // only enable hot in development
+              hmr: process.env.NODE_ENV === "development",
+              // if hmr does not work, this is a forceful method.
+              reloadAll: true,
+            },
+          },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: {
+                localIdentName: "[name]__[local]--[hash:base64:5]",
+              },
+              localsConvention: "camelCaseOnly",
+            },
+          },
+          "postcss-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Application",
+    }),
+    new MiniCssExtractPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "public" }],
+    }),
+    new Dotenv({
+      path: process.env.DOTENV || "./.env",
+    }),
+  ],
+  resolve: {
+    extensions: [".js", ".ts", ".tsx", ".json"],
+    alias: {
+      src: path.resolve(__dirname, "../../src/"),
+    },
+  },
+  }
