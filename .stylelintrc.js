c132340e2c08ffae686d15685f36bdf4e69ad7c4
Timur Vorobyov <buskettim0902@gmail.com>
2021-11-04 15:09:29 +0500

    chore: installs prettier

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     "stylelint-config-standard",
     "stylelint-config-css-modules",
     "stylelint-config-idiomatic-order",
+    "stylelint-config-prettier",
   ],
   rules: {
     "at-rule-no-unknown": null,
