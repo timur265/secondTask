95d40f6fb829625f7c12c30ef594cb5bf4553b12
Timur Vorobyov <buskettim0902@gmail.com>
2021-11-04 08:21:32 +0500

    chore: creates BodyWrapper component

---
+++ src/wrappers/AppWrapper/BodyWrapper/BodyWrapper.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Switch } from "react-router-dom";
+
+
+function BodyWrapper() {
+  return (
+    <BrowserRouter>
+      <Switch>
+
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default BodyWrapper;
