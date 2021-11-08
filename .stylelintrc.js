2e87b6ce4571fa31e74117c0227cb0f9e262531f
Timur Vorobyov <buskettim0902@gmail.com>
2021-11-08 20:38:09 +0500

    chore: adds README

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
   extends: [
-    "stylelint-config-standard",
-    "stylelint-config-css-modules",
-    "stylelint-config-idiomatic-order",
-    "stylelint-config-prettier",
+    "stylelint-config-standard", //Turns on additional rules to enforce common conventions found in the specifications and in a handful of CSS styleguides, including: The Idiomatic CSS Principles, Google's CSS Style Guide, Airbnb's Styleguide, and @mdo's Code Guide.
+    "stylelint-config-css-modules", //Tweaks stylelint rules to accept css modules specific syntax. This is useful as an override of pre-defined rules, for instance the stylelint-config-standard.
+    "stylelint-config-idiomatic-order", //Order your styles based on idiomatic-css.
+    "stylelint-config-prettier", //Turns off all rules that are unnecessary or might conflict with Prettier. This lets you use your favorite shareable config without letting its stylistic choices get in the way when using Prettier.
   ],
   rules: {
-    "at-rule-no-unknown": null,
-    "no-descending-specificity": null,
+    "at-rule-no-unknown": null, //Disallow unknown at-rules.
+    "no-descending-specificity": null, //Disallow selectors of lower specificity from coming after overriding selectors of higher specificity.
   },
   ignoreFiles: ["!**/*.css"],
 };
